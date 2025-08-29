9611fde448418eaaafb4948d560f048217a467a4
m-hamidmehmood <saim008865@gmail.com>
2025-08-29 11:18:05 +0500

    refactor: update product and products page to use resolved parameters

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,10 +1,13 @@
-import ProductsContainer from '@/components/product/ProductsContainer';
+import ProductsContainer from '@/components/product/ProductsContainer'
 import React from 'react'
 
-export default function ProductsPage({searchParams}: {searchParams: {layout?:string;search?:string}}) {
-  const layout=searchParams.layout || 'grid';
-  const search=searchParams.search || '';
-  return (
-  <ProductsContainer layout={layout} search={search}/>
-  )
+export default async function ProductsPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ layout?: string; search?: string }>
+}) {
+  const resolvedSearchParams = await searchParams
+  const layout = resolvedSearchParams.layout || 'grid'
+  const search = resolvedSearchParams.search || ''
+  return <ProductsContainer layout={layout} search={search} />
 }
